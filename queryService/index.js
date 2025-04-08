7b896f4f724c98d50f2269022ce1687b6dccd839
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2025-04-08 16:17:00 +0530

    query service tp store posts and comments together

---
+++ queryService/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended:true})); 
+app.use(cors());
+
+const port = 3003;
+const commentsByPostID={};
+let id=0;
+const posts={};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+
+//event lsitner
+app.post('/events',(req, res) => {
+    const {type, data} = req.body;
+    if(type==="postCreated"){
+        const {id, text}=data;
+        posts[id]={id, text,comments:[]};
+    }
+    else if(type==="commentCreated"){
+        const {id, postId, text}=data;
+        posts[postId].comments.push({id, text});
+    }
+    res.send("ok");
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
