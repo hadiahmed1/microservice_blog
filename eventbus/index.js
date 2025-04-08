2dcd8375ec09881527a43894a88ca78228ec24bb
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2025-04-08 15:48:43 +0530

    microservice to listen to and broadcasst events

---
+++ eventbus/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended:true})); 
+app.use(cors());
+
+const port = 3005;
+app.post('/events', (req, res) => {
+    const event = req.body;
+    // axios.post("http://localhost:3001/events", event);
+    axios.post("http://localhost:3002/events", event);//posts
+    axios.post("http://localhost:3001/events", event);//comments
+
+    res.send("Ok");
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
